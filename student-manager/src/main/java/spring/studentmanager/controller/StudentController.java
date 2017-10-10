2cee4ff7eeff903c7e96e669d4db82d4b8c5f848
Taras Vladyka <tarasvladyka@gmail.com>
2017-10-10 03:23:15 +0300

    Added spring boot task - student-manager

---
+++ student-manager/src/main/java/spring/studentmanager/controller/StudentController.java
@@ -0,0 +1,70 @@
+package spring.studentmanager.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.studentmanager.Student;
+
+@RestController
+public class StudentController {
+	private static List<Student> students = new LinkedList<>();
+	
+	@RequestMapping(method=RequestMethod.POST, path="/students")
+	public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+		int studentId = students.size();
+		student.setId(studentId);
+		students.add(student);
+		return new ResponseEntity<>(student, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="/students/{studentId}")
+	public ResponseEntity<Student> findById(@PathVariable int studentId) {
+		Student foundStudent = students.stream()
+				.filter(s->s.getId()==studentId)
+				.findFirst().orElse(null);
+		if(foundStudent != null) {
+			return new ResponseEntity<>(foundStudent, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	@RequestMapping(method=RequestMethod.GET, path="/students/")
+	public ResponseEntity<List<Student>> findAll() {
+		if(!students.isEmpty()) {
+			return new ResponseEntity<>(students, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	@RequestMapping(method=RequestMethod.DELETE, path="/students/{studentId}")
+	public ResponseEntity<Student> deleteById(@PathVariable int studentId) {
+		Student student = students.stream()
+				.filter(s->s.getId()==studentId)
+				.findFirst().orElse(null);
+		if(student != null) {
+			students.removeIf(s->s.getId()==studentId);
+			return new ResponseEntity<>(student, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	@RequestMapping(method=RequestMethod.PUT, path="/students/{studentId}")
+	public ResponseEntity<Student> updateById(@PathVariable int studentId, @RequestBody Student newStudent) {
+		Student student = students.stream()
+				.filter(s->s.getId()==studentId)
+				.findFirst().orElse(null);
+		if(student != null) {
+			student.setFirstName(newStudent.getFirstName());
+			student.setLastName(newStudent.getLastName());
+			student.setInstName(newStudent.getInstName());
+			student.setCourseNum(newStudent.getCourseNum());
+			return new ResponseEntity<>(student, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+}
